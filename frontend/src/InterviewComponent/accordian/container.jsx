8dd0ccd27054b216131171bb651acf46033976b5
Dipanshu <deepanshu.patil69@gmail.com>
2024-02-28 19:56:35 +0530

    completed Accordian

--- frontend/src/InterviewComponent/accordian/container.jsx
+++ frontend/src/InterviewComponent/accordian/container.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./container.css";
 
-const Container = ({ item }) => {
-  const [showAnswer, setShowAnswer] = useState(false);
+const Container = ({
+  item,
+  id,
+  setSelected,
+  selected,
+  multiSelect,
+  setMultiSelect,
+  enableMultiSelect,
+}) => {
   const { question, answer } = item;
 
+  const handleSelection = (id) => {
+    setSelected(id === selected ? null : id);
+  };
+
+  const handleMultiSelection = (id) => {
+    const newArray = [...multiSelect];
+    const findIndexOfCurrentId = newArray.indexOf(id);
+    if (findIndexOfCurrentId === -1) newArray.push(id);
+    else newArray.splice(findIndexOfCurrentId, 1);
+    setMultiSelect(newArray);
+  };
+
+
+
   return (
-    <div className="infoContainer">
+    <div
+      className="infoContainer"
+      onClick={
+        enableMultiSelect
+          ? () => handleMultiSelection(id)
+          : () => handleSelection(id)
+      }
+    >
       <div className="subInfo">
         <h4>{question}</h4>
-        <button onClick={() => setShowAnswer(!showAnswer)}>+</button>
+        <span>+</span>
       </div>
-      {showAnswer ? <p>{answer}</p> : null}
+      {enableMultiSelect
+        ? multiSelect.indexOf(id) !== -1 && <p>{answer}</p>
+        : selected === id && <p>{answer}</p>}
     </div>
   );
 };
--- frontend/src/InterviewComponent/accordian/index.jsx
+++ frontend/src/InterviewComponent/accordian/index.jsx
@@ -5,15 +5,34 @@ import Container from "./container";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [word, setWord] = useState("Enable");
+  const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+  const [multiSelect, setMultiSelect] = useState([]);
+
+  const handleClick = () => {
+    word === "Enable" ? setWord("Disabled") : setWord("Enable");
+    setEnableMultiSelect(!enableMultiSelect);
+  };
 
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           <div>
-            <button>Enable Multi Selection</button>
+            <button onClick={handleClick}>{`${word} Multi Selection`}</button>
             {data.map((item, index) => {
-              return <Container item={item} key={item.id} />;
+              return (
+                <Container
+                  item={item}
+                  key={item.id}
+                  id={item.id}
+                  setSelected={setSelected}
+                  selected={selected}
+                  multiSelect={multiSelect}
+                  setMultiSelect={setMultiSelect}
+                  enableMultiSelect={enableMultiSelect}
+                />
+              );
             })}
           </div>
         ) : null}
